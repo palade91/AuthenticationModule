8e9d169c203a900fae13e2e605f4f2f12f2df5a5
Catalin Palade <palade91@yahoo.com>
2022-09-24 10:11:44 +0100

    WIP

--- Authentication/Sources/Authentication/AuthenticationView.swift
+++ Authentication/Sources/Authentication/AuthenticationView.swift
@@ -13,7 +13,9 @@ public struct AuthenticationView: View {
     public init() {}
     
     public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            LoginView()
+        }
     }
 }
 
---
+++ Authentication/Sources/Authentication/SwiftUIView.swift
@@ -0,0 +1,25 @@
+//
+//  LoginView.swift
+//  
+//
+//  Created by Catalin Palade on 24/09/2022.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct LoginView: View {
+   
+    public init() { }
+    
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+@available(iOS 13.0, *)
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
